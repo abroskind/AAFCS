aa41b793e46945c5cf36263d99ce68cb4ea1a4f1
Dmytro Abroskin <abroskind@icloud.com>
2023-09-13 19:45:37 +0100

    Initial Commit

---
+++ AAFCS/AAFCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AAFCSApp.swift
+//  AAFCS
+//
+//  Created by Dmytro Abroskin on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AAFCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
